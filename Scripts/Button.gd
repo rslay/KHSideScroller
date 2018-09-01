92ca4738e62f6d15b5b2a342c224cb53cbb25503
SilentCipher1997 <brandon.torrens001@mymdc.net>
2018-09-01 13:07:42 -0400

    Made sprites transparent, organized, added main menu

---
+++ Scripts/Button.gd
@@ -0,0 +1,15 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scenes/Level1.tscn")
